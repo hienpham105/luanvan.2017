bd284d1a5a73a52bf8d4e1f48fd617bbed3a6800
phạm văn hiền <pherohien.pham@gmail.com>
2017-04-08 14:12:09 +0700

    Add project files.

---
+++ luanvanWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(luanvanWeb.Startup))]
+namespace luanvanWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
